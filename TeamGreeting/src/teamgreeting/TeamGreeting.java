990974b053c2668535065632f84e24c668e1cbc4
harshgujarati <gujarati.h@northeastern.edu>
2025-03-11 13:23:52 -0400

    Member 1 done

---
+++ TeamGreeting/src/teamgreeting/TeamGreeting.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package teamgreeting;
+
+/**
+ *
+ * @author harsh
+ */
+public class TeamGreeting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Member1.main(args);
+    }
+    
+}
